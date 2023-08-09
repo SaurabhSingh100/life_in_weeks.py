7ad8de633a00ca8d9efc5f80e7898ff609a11c21
Saurabh Singh <108789534+SaurabhSingh100@users.noreply.github.com>
2023-08-09 12:41:58 +0530

    Create life_is_left.py

---
+++ life_is_left.py
@@ -0,0 +1,12 @@
+is_true = True
+while is_true:
+    age = (input("enter your age"))
+    if age.lower() == "off":
+        print("thankyou")
+        break
+    age_as_int = int(age)
+    year = (90 - age_as_int)
+    days_left = (year * 365)
+    week_left = (year * 52)
+    months_left = (year * 12)
+    print(f"you have {days_left} days, {week_left} weeks and ,{months_left} months left.")
